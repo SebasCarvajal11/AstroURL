b134c138206c96aa9aab650ada4a83002ecb469c
SebasCarvajal11 <131422103+SebasCarvajal11@users.noreply.github.com>
2026-03-26 11:11:22 -0500

    Add health check endpoint

---
+++ src/main/java/com/astrourl/api/controller/HealthCheckController.java
@@ -0,0 +1,13 @@
+package com.astrourl.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "OK";
+    }
+}
